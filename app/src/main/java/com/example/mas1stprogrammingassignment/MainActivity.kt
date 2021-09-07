17dfbae0387fd63bb4c9d445ba56d73656b654a9
Freya Wang <wangfreya582@gmail.com>
2021-09-07 16:32:01 -0400

    Fixed build issues + testing button

--- app/src/main/java/com/example/mas1stprogrammingassignment/MainActivity.kt
+++ app/src/main/java/com/example/mas1stprogrammingassignment/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.mas1stprogrammingassignment
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +18,7 @@ class MainActivity : AppCompatActivity() {
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+            Log.i("Did something", "I pushed a button")
         }
     }
 
