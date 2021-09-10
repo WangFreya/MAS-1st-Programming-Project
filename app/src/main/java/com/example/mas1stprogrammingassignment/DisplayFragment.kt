a0f2f54045a8e4a8fb6a08c81a95b54717aed99b
Freya Wang <wangfreya582@gmail.com>
2021-09-10 13:32:27 -0400

    Added display fragment + xml Fixed bug

--- app/src/main/java/com/example/mas1stprogrammingassignment/DisplayFragment.kt
+++ app/src/main/java/com/example/mas1stprogrammingassignment/DisplayFragment.kt
@@ -11,21 +11,21 @@ import androidx.navigation.fragment.findNavController
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FormFragment : Fragment() {
+class DisplayFragment : Fragment() {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.form, container, false)
+        return inflater.inflate(R.layout.display, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_submit).setOnClickListener {
-            findNavController().navigate(R.id.action_formFragment_to_displayFragment)
+        view.findViewById<Button>(R.id.button_display_next).setOnClickListener {
+            findNavController().navigate(R.id.action_displayFragment_to_SecondFragment)
         }
     }
 }
