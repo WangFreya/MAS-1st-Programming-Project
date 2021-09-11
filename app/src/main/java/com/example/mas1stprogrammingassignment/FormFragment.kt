81ec57df093a259aa7ea0f209e00bf74a521286f
Freya Wang <wangfreya582@gmail.com>
2021-09-11 13:09:08 -0400

    Changed radio buttons to text input for ease. User input saved in variables within form fragment

--- app/src/main/java/com/example/mas1stprogrammingassignment/FormFragment.kt
+++ app/src/main/java/com/example/mas1stprogrammingassignment/FormFragment.kt
@@ -1,23 +1,31 @@
 package com.example.mas1stprogrammingassignment
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.form.*
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class FormFragment : Fragment() {
 
+    private var fullName = ""
+    private var age = 0;
+    private var searchText = ""
+    private var usedWikiBefore = ""
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.form, container, false)
     }
 
@@ -25,7 +33,13 @@ class FormFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         view.findViewById<Button>(R.id.button_submit).setOnClickListener {
+            this.fullName = name.text.toString()
+            this.age = ageOfUser.text.toString().toInt()
+            this.searchText= multiLine.text.toString()
+            this.usedWikiBefore = usedWiki.text.toString()
+
             findNavController().navigate(R.id.action_formFragment_to_displayFragment)
         }
+
     }
 }
